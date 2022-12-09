e6c1457b6caf8cfc60cba3345b0cad8efc8bc87e
hanmariyang <hanmari.code@gmail.com>
2022-12-10 03:00:09 +0900

    [추가] 검색어 실시간 랭킹 기능
    1. 1~10등까지 검색어 롤링배너 추가

--- js/index_api.js
+++ js/index_api.js
@@ -56,6 +56,21 @@ function timeForToday(value) {
 }
 
 
+// 인기 검색어 랭킹 조회
+async function getHeaderSearchWordRanking(){
+    const response = await fetch(`${backEndBaseUrl}/communities/search/word/ranking/`,{
+        headers: {
+            'content-type': 'application/json',
+            "Authorization":"Bearer " + localStorage.getItem("access")
+        },
+        method:'GET',
+    })
+
+    response_json = await response.json()
+    return response_json
+}
+
+
 
 window.onload = async function getIndex_API(){
     let User_payload = JSON.parse(localStorage.getItem('payload'))
@@ -133,6 +148,33 @@ window.onload = async function getIndex_API(){
                 `
             })
         })
-        
+
+
+    // 검색어 랭킹 조회
+    search_word_list = await getHeaderSearchWordRanking()
+    search_word_list = search_word_list.sort((a, b) => b.count - a.count)
+
+    var word_rank_01 = document.getElementsByClassName('rank_01')[0];
+    var word_rank_02 = document.getElementsByClassName('rank_02')[0];
+    var word_rank_03 = document.getElementsByClassName('rank_03')[0];
+    var word_rank_04 = document.getElementsByClassName('rank_04')[0];
+    var word_rank_05 = document.getElementsByClassName('rank_05')[0];
+    var word_rank_06 = document.getElementsByClassName('rank_06')[0];
+    var word_rank_07 = document.getElementsByClassName('rank_07')[0];
+    var word_rank_08 = document.getElementsByClassName('rank_08')[0];
+    var word_rank_09 = document.getElementsByClassName('rank_09')[0];
+    var word_rank_10 = document.getElementsByClassName('rank_10')[0];
+
+    word_rank_01.innerText = `1등 : ${search_word_list[0]['word']}`
+    word_rank_02.innerText = `2등 : ${search_word_list[1]['word']}`
+    word_rank_03.innerText = `3등 : ${search_word_list[2]['word']}`
+    word_rank_04.innerText = `4등 : ${search_word_list[3]['word']}`
+    word_rank_05.innerText = `5등 : ${search_word_list[4]['word']}`
+    word_rank_06.innerText = `6등 : ${search_word_list[5]['word']}`
+    word_rank_07.innerText = `7등 : ${search_word_list[6]['word']}`
+    word_rank_08.innerText = `8등 : ${search_word_list[7]['word']}`
+    word_rank_09.innerText = `9등 : ${search_word_list[8]['word']}`
+    word_rank_10.innerText = `10등 : ${search_word_list[9]['word']}`
+
 }
 }
