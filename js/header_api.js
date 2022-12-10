bcca8337789f489ba89625797492d169a62e0212
Sung_cn_2 <112399821+SungChangNam@users.noreply.github.com>
2022-12-10 19:36:30 +0900

    [테스트]

--- js/header_api.js
+++ js/header_api.js
@@ -10,15 +10,15 @@ async function handleLogout() {
 
 
 
-// 유저 네임 보이기
-window.onload =()=>  {
-    const payload = localStorage.getItem("payload");
-    const payload_parse =JSON.parse(payload)
-    console.log(payload_parse.username)
+// // 유저 네임 보이기
+// window.onload =()=>  {
+//     const payload = localStorage.getItem("payload");
+//     const payload_parse =JSON.parse(payload)
+//     console.log(payload_parse.username)
 
 
-    const intro = document.getElementById("intro")
-    intro.innerText = `${payload_parse.username}`
+//     const intro = document.getElementById("intro")
+//     intro.innerText = `${payload_parse.username}`
 
 
-} 
+// } 
