a6b83efe68f9bd8f3e6581098f84a782ad7a88a1
hanmariyang <hanmari.code@gmail.com>
2022-12-13 06:09:17 +0900

    [수정] nav.html 메뉴 버튼 css 업데이트

--- js/index.js
+++ js/index.js
@@ -36,12 +36,19 @@ function brandOn(){
         $("#nav_main_brand_right").css("display", "none");
         $("#nav_main_bt_right").css("background-color", "#ffffff");
         $("#nav_main_bt_right").css("color", "#000000");
+
+        $("#nav_main_category_left").css("display", "flex");
+        $("#nav_main_bt_left").css("background-color", "#000000");
+        $("#nav_main_bt_left").css("color", "#ffffff");
     }else{
         // 브랜드 온
         $("#nav_main_brand_right").css("display", "flex");
-        $("#nav_main_brand_right").css("position", "absolute");
         $("#nav_main_bt_right").css("background-color", "#000000");
         $("#nav_main_bt_right").css("color", "#ffffff");
+
+        $("#nav_main_category_left").css("display", "none");
+        $("#nav_main_bt_left").css("background-color", "#ffffff");
+        $("#nav_main_bt_left").css("color", "#000000");
     }
 }
 
@@ -53,11 +60,18 @@ function categoryOn(){
         $("#nav_main_category_left").css("display", "none");
         $("#nav_main_bt_left").css("background-color", "#ffffff");
         $("#nav_main_bt_left").css("color", "#000000");
+
+        $("#nav_main_brand_right").css("display", "flex");
+        $("#nav_main_bt_right").css("background-color", "#000000");
+        $("#nav_main_bt_right").css("color", "#ffffff");
     }else{
         // 카테고리 온
         $("#nav_main_category_left").css("display", "flex");
-        $("#nav_main_category_left").css("position", "absolute");
         $("#nav_main_bt_left").css("background-color", "#000000");
         $("#nav_main_bt_left").css("color", "#ffffff");
+
+        $("#nav_main_brand_right").css("display", "none");
+        $("#nav_main_bt_right").css("background-color", "#ffffff");
+        $("#nav_main_bt_right").css("color", "#000000");
     }
 }
