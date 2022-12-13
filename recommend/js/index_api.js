3aef981d13ac68b266512d7361e396c979f76651
노우석 <nhowooseok@gmail.com>
2022-12-13 22:21:58 +0900

    [추가]현재 탬플릿 각종 버튼 연결 추가

--- recommend/js/index_api.js
+++ recommend/js/index_api.js
@@ -186,7 +186,9 @@ window.onload = async function getIndex_API(){
     var AttendanceCheck = document.getElementById('AttendanceCheck')
     AttendanceCheck.setAttribute('onclick',`AttendanceCheck(${User_payload.user_id})`)
 
-
+    // 옷장 버튼
+    var hd_closet_button = document.getElementById('header_closet_button')
+    hd_closet_button.setAttribute('href', `/products/closet/?user_id=${User_payload.user_id}`)
 
 
     // 검색어 랭킹 조회
