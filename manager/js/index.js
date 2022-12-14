3b57ce27d343385dae33fca595e799ebc8be03e5
노우석 <nhowooseok@gmail.com>
2022-12-14 18:09:13 +0900

    [수정]날씨 정보 업데이트 alert창 수정

--- manager/js/index.js
+++ manager/js/index.js
@@ -57,6 +57,7 @@ async function productUpdate() {
 
 // 날씨 정보 업데이트
 async function weatherUpdate() {
+  console.log("hi")
   const response = await fetch(`${backEndBaseUrl}/weather/`, {
     headers: {
       Authorization: "Bearer " + localStorage.getItem("access"),
@@ -68,7 +69,7 @@ async function weatherUpdate() {
   response_json = await response.json();
 
   if (response.status == 200) {
-    alert(response_json["message"]);
+    alert("날씨 정보가 업데이트 되었습니다");
   }
   return response_json;
 }
