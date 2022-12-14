dfb4900100ba8fdeea43514bd32e4a69cacdf217
hanmariyang <hanmari.code@gmail.com>
2022-12-14 12:35:01 +0900

    [수정] header 부분 로그아웃 버튼 활성화

--- users/js/header_api.js
+++ users/js/header_api.js
@@ -1,6 +1,3 @@
-const frontEndBaseUrl = "http://127.0.0.1:5500"
-const backEndBaseUrl = "http://127.0.0.1:8000"
-
 
 // 회원 정보 조회 API
 async function getUserHeaderInfo(){
@@ -17,30 +14,11 @@ async function getUserHeaderInfo(){
     return response_json
 }
 
-window.onload = async function getUserHeader_API(){
-    console.log("hi")
-    feed_list = await getUserHeaderInfo()
-
-    console.log(header_user)
-
-    var profile_nickname = document.getElementsByClassName('profile_nickname')[0];
-    var profile_tier_info = document.getElementsByClassName('profile_tier_info')[0];
-    var profile_created_at = document.getElementsByClassName('profile_created_at')[0];
-    var profile_next_tier_info = document.getElementsByClassName('profile_next_tier_info')[0];
-    var follow_value = document.getElementsByClassName('follow_value')[0];
-    var follower_value = document.getElementsByClassName('follower_value')[0];
-    var feed_value = document.getElementsByClassName('feed_value')[0];
-    var closet_value = document.getElementsByClassName('closet_value')[0];
-
-    f
-
-
-
-
-
-
-
-
-
-
+// 로그아웃 버튼
+async function handleLogout() {
+    localStorage.removeItem("access");
+    localStorage.removeItem("refresh");
+    localStorage.removeItem("payload");
+    alert("로그아웃 되었습니다");
+    window.location.replace(`${frontEndBaseUrl}/users/login.html`);
 }
