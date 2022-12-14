e2682dff2af4e5e4608aca4102990d51bc3a5ba0
노우석 <nhowooseok@gmail.com>
2022-12-14 14:16:48 +0900

    [삭제]header.api 필요없는 코드 삭제

--- users/js/header_api.js
+++ users/js/header_api.js
@@ -14,27 +14,9 @@ async function getUserHeaderInfo(){
     return response_json
 }
 
-window.onload = async function getUserHeader_API(){
-    feed_list = await getUserHeaderInfo()
-
-    var profile_nickname = document.getElementsByClassName('profile_nickname')[0];
-    var profile_tier_info = document.getElementsByClassName('profile_tier_info')[0];
-    var profile_created_at = document.getElementsByClassName('profile_created_at')[0];
-    var profile_next_tier_info = document.getElementsByClassName('profile_next_tier_info')[0];
-    var follow_value = document.getElementsByClassName('follow_value')[0];
-    var follower_value = document.getElementsByClassName('follower_value')[0];
-    var feed_value = document.getElementsByClassName('feed_value')[0];
-    var closet_value = document.getElementsByClassName('closet_value')[0];
-
-    f
 
 
 
 
 
 
-
-
-
-
-}
